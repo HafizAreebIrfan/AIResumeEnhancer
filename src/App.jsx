ffc3b769fd6b6719be7b0309b06f91bdc48456f1
Hafiz Areeb <hafizareebirfan@gmail.com>
2025-09-07 01:04:25 +0500

    speed insights by vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./app.css";
 import AIResumeEnhancer from "./components/airesumeenhancer";
 import Home from "./components/home";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function App() {
   return (
     <>
       <Home />
       <AIResumeEnhancer/>
+      <SpeedInsights/>
     </>
   );
 }
