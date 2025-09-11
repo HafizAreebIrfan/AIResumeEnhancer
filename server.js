e74fc28f917b07ee86601e0d1c95922704527525
Hafiz Areeb <hafizareebirfan@gmail.com>
2025-09-11 20:08:21 +0500

    logs added

--- server.js
+++ server.js
@@ -11,14 +11,19 @@ app.use(
   })
 );
 app.use(express.json());
-const port = process.env.PORT;
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res) => {
+  console.log("Root route hit!");
+  res.send("Server running on Vercel");
+});
 
 app.get("/", (req, res) => {
   res.send("Server running");
 });
 app.use("/api", ApiCall);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
 module.exports = app;
